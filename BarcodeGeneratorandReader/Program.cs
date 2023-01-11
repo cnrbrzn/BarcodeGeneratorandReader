5799b9ef1d9ce385d054bd38aaf25604132648db
Caner Borazan <caner.borazan@lunatr.com>
2023-01-11 14:23:50 +0300

    Add project files.

---
+++ BarcodeGeneratorandReader/Program.cs
@@ -0,0 +1,16 @@
+﻿//3rd party barcode kütüphaneleri kullanarak barcode üreten bunu client'ın bilgisayarında bir lokasyona kaydeden. Daha sonra da barcode'u okuyabilen bir console uygulaması yazınız.
+
+//Kullanılması gereken teknikler:
+
+//Console Application
+//Import 3rd Party Library
+//Dosyaya Yazma
+//Dosyadan Okuma
+
+using BarcodeGeneratorandReader;
+using BarcodeLib;
+using System.Drawing;
+
+Console.WriteLine("Bir barkod değeri giriniz: ");
+Barcode barcode = Process.CreateAndSaveBarcode(Console.ReadLine());
+Console.WriteLine(Process.ReadBarcode(barcode));
